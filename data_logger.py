bc56cbf8e54b15db5ed695a588090fe391ea32e5
Winslow <winslow@clearcreeksci.com>
2025-09-23 22:33:19 -0400

    Added deployment and license

--- data_logger.py
+++ data_logger.py
@@ -17,9 +17,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-*********************************
-    Requires various Python packages. See the requirements.txt file.
-
 """
 import os
 import argparse
--- deployment/build_bundle.sh
+++ deployment/build_bundle.sh
@@ -7,7 +7,7 @@ fi
 
 ./make_manifest.sh $VERSION > ./manifest.xml
 
-zip -r "${PREFIX}_version_${VERSION}.zip" ./manifest.xml ../data_logger.py ../requirements.txt ../plugins/*.py ../system/ccsweatherdatalogger.service
+zip -r "${PREFIX}_version_${VERSION}.zip" ./manifest.xml ../data_logger.py ../requirements.txt ../plugins/*.py ../ccs_dlconfig/*.py ../system/ccsweatherdatalogger.service
 
 
 
