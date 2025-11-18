d057fba5834e199c2dbe456bdc8d10792a5930d3
Winslow <winslow@clearcreeksci.com>
2025-11-18 05:54:55 -0500

    added command line config path

--- deployment/build_bundle.sh
+++ deployment/build_bundle.sh
@@ -1,5 +1,5 @@
 VERSION="NotForRelease"
-PREFIX="WeatherDataLogger_Install_Bundle"
+PREFIX="DataLogger_Install_Bundle"
 
 if [ $# -eq 1 ]; then
     VERSION="$1"
@@ -7,7 +7,7 @@ fi
 
 ./make_manifest.sh $VERSION > ./manifest.xml
 
-zip -r "${PREFIX}_version_${VERSION}.zip" ./manifest.xml ../data_logger.py ../plugins/*.py ../ccs_dlconfig/*.py ./system/ccsweatherdatalogger.service
+zip -r "${PREFIX}_v${VERSION}.zip" ./manifest.xml ../data_logger.py ../plugins/*.py ../ccs_dlconfig/*.py ./system/ccsweatherdatalogger.service
 
 
 
