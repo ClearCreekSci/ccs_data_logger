59366c3db466da0493db63bcf860dd39c78f5b23
Winslow <winslow@clearcreeksci.com>
2026-01-30 06:00:26 -0500

    subclass settings

--- data_logger.py
+++ data_logger.py
@@ -31,12 +31,16 @@ from ccs_dlconfig import config
 import xml.etree.ElementTree as et
 
 SHARED_OBJECT_DIR                   = 'plugins'
+
 # For the default period of 30 minutes between events, collecting 48 events gives
 # a one day default file rollover...
 DEFAULT_COLLECT_PERIOD              = 30
-DEFAULT_COLLECT_EVENTS              = 48
+DEFAULT_ROLLOVER_COUNT              = 48
 COLLECT_SUFFIX                      = '_ccs_logger.csv'
 
+TAG_LOG_EVENT = 'log-event'
+TAG_PERIOD = 'period'
+TAG_ROLLOVER_COUNT = 'rollover-count'
 
 logging.basicConfig(filename='/tmp/data_logger.log')
 logging.basicConfig(level=logging.INFO)
@@ -45,11 +49,47 @@ log.setLevel(logging.INFO)
 g_done = False
 g_config = None
 
+class LoggerSettings(config.Settings):
+
+    def __init__(self):
+        super().__init__()
+        self.period = DEFAULT_COLLECT_PERIOD
+        self.rollover_count = DEFAULT_ROLLOVER_COUNT
+
+    def set_period(self,v):
+        self.period = v
+
+    def set_rollover_count(self,v):
+        self.rollover_count = v
+
+    def read(self,path):
+        try:
+            super().read(path)
+            for collect_node in self.root.findall(TAG_LOG_EVENT):
+                period = collect_node.find(TAG_PERIOD)
+                if None is not period:
+                    self.period = int(period.text.strip())
+                count = collect_node.find(TAG_ROLLOVER_COUNT)
+                if None is not count:
+                    self.rollover_count = int(count.text.strip())
+        except FileNotFoundError as ex:
+            self.write(path)
+            self.read(path)
+
+    def write(self,path):
+        with open(path,'wt') as fd:
+            self.write_prefix(fd)
+            fd.write('<' + TAG_LOG_EVENT + '>\n')
+            fd.write('<' + TAG_PERIOD + '>' + str(self.period) + '</' + TAG_PERIOD + '>\n')
+            fd.write('<' + TAG_ROLLOVER_COUNT + '>' + str(self.rollover_count) + '</' + TAG_ROLLOVER_COUNT + '>\n')
+            fd.write('</' + TAG_LOG_EVENT + '>\n')
+            self.write_suffix(fd)
+
+
 class CcsLogger(object):
 
     def __init__(self):
         self.load_plugins()
-
 
     def load_plugins(self):
         self.plugins = list()
@@ -97,7 +137,7 @@ class CcsLogger(object):
             s += ',' + str(x[0])
         return s
 
-def get_path(dst):
+def get_collect_file_path(dst):
     ts = datetime.utcnow()
     name = ts.strftime('%Y%m%d%I%M%S') + COLLECT_SUFFIX
     return os.path.join(dst,name)
@@ -106,38 +146,34 @@ def run(args):
     global g_done
     global g_config
 
-    try:
-        g_config = config.Settings(args.config,raise_exceptions=True)
-        args.period = g_config.frequency
-        args.events = g_config.package_rate
-    except FileNotFoundError as e:
-        print('Ignoring exception: ' + str(e))
+    g_config = LoggerSettings()
+    g_config.read(args.config)
+
+    if None is not args.period:
+        g_config.set_period(args.period)
+    if None is not args.events:
+        g_config.set_rollver_count(args.events)
 
     data_logger = CcsLogger()
     count = 0
-    path = get_path(args.destination)
+    path = get_collect_file_path(g_config.data_dir)
     print('path: ' + path + ', period: ' + str(args.period) + ', events: ' + str(args.events))
     while False == g_done:
         data_logger.collect(path)
-        time.sleep(60 * args.period)
+        time.sleep(60 * g_config.period)
         count += 1
-        if count >= args.events:
-            path = get_path(args.destination)
+        if count >= g_config.rollover_count:
+            # Get a new file with updated timestamp
+            path = get_collect_file_path(g_config.data_dir)
             count = 0
 
 
 if '__main__' == __name__:
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('-c','--config',required=True,help='Path to config file')
-    arg_parser.add_argument('-d','--destination',required=True,help='Directory into which data is stored')
     arg_parser.add_argument('-p','--period',type=int,help='Number of minutes between collection events, default is 30')
     arg_parser.add_argument('-e','--events',type=int,help='Number of collection events to store in each file, default is 48')
     args = arg_parser.parse_args()
-    if None is args.period:
-        args.period = int(DEFAULT_COLLECT_PERIOD)
-    if None is args.events:
-        args.events = int(DEFAULT_COLLECT_EVENTS)
-
     run(args)
 
 
--- deployment/build_bundle.sh
+++ deployment/build_bundle.sh
@@ -7,7 +7,8 @@ fi
 
 ./make_manifest.sh $VERSION > ./manifest.xml
 
-zip -r "${PREFIX}_v${VERSION}.zip" ./manifest.xml ../data_logger.py ../plugins/*.py ../ccs_dlconfig/*.py ./system/ccsweatherdatalogger.service
+zip -r "${PREFIX}_v${VERSION}.zip" ./settings.cfg ./manifest.xml ../data_logger.py ../plugins/*.py ../ccs_dlconfig/*.py ./system/ccsweatherdatalogger.service
+
 
 
 
