0dff84cc087f9b51fe765c1319cceac3ba3cd743
Winslow <winslow@clearcreeksci.com>
2026-02-09 03:19:56 -0500

    move to ccs_base

--- data_logger.py
+++ data_logger.py
@@ -24,12 +24,13 @@ import logging
 import time
 
 from importlib import import_module
-from datetime import datetime
+import datetime
 
 from ccs_dlconfig import config
 
 import xml.etree.ElementTree as et
 
+NAME                                = 'data_logger'
 SHARED_OBJECT_DIR                   = 'plugins'
 
 # For the default period of 30 minutes between events, collecting 48 events gives
@@ -37,59 +38,84 @@ SHARED_OBJECT_DIR                   = 'plugins'
 DEFAULT_COLLECT_PERIOD              = 30
 DEFAULT_ROLLOVER_COUNT              = 48
 COLLECT_SUFFIX                      = '_ccs_logger.csv'
+LOG_SUFFIX                          = '_ccs_data_logger.log'
 
-TAG_LOG_EVENT = 'log-event'
-TAG_PERIOD = 'period'
+TAG_PERIOD         = 'period'
+TAG_PLUGIN         = 'plugin'
+TAG_PLUGINS        = 'plugins'
+TAG_PLUGIN_CONFIG  = 'plugin-config'
 TAG_ROLLOVER_COUNT = 'rollover-count'
+TAG_SCHEDULE       = 'schedule'
 
-logging.basicConfig(filename='/tmp/data_logger.log')
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
+MAX_PLUGIN_REPORTED_ERRORS = 4
+
 g_done = False
 g_config = None
+
+def logmsg(tag,msg):
+    global g_config
+    if hasattr(g_config,'log_path') and None is not g_config.log_path:
+        with open(g_config.log_path,'a') as fd:
+            ts = datetime.datetime.now(datetime.UTC)
+            s = ts.strftime('%Y-%m-%d %I:%M:%S ') + '[' + tag + '] ' + msg + '\n'
+            fd.write(s)
+
+class PluginMeta(object):
+
+    def __init__(self):
+        self.name = None
+        self.active = False
+        self.ticks = 0
+        self.period = 0
+        self.rollover_max = 0
+        self.rollover_count = 0
+        self.path = None
+        self.config = None
 
 class LoggerSettings(config.Settings):
 
     def __init__(self):
         super().__init__()
-        self.period = DEFAULT_COLLECT_PERIOD
-        self.rollover_count = DEFAULT_ROLLOVER_COUNT
-
-    def set_period(self,v):
-        self.period = v
-
-    def set_rollover_count(self,v):
-        self.rollover_count = v
+        self.plugins = list()
+        self.log_path = None
 
     def read(self,path):
-        try:
-            super().read(path)
-            for collect_node in self.root.findall(TAG_LOG_EVENT):
-                period = collect_node.find(TAG_PERIOD)
-                if None is not period:
-                    self.period = int(period.text.strip())
-                count = collect_node.find(TAG_ROLLOVER_COUNT)
-                if None is not count:
-                    self.rollover_count = int(count.text.strip())
-        except FileNotFoundError as ex:
-            self.write(path)
-            self.read(path)
+        super().read(path)
+        if None is not self.log_dir:
+            ts = datetime.datetime.now(datetime.UTC)
+            name = ts.strftime('%Y%m%d%I%M%S') + LOG_SUFFIX
+            self.log_path = os.path.join(g_config.log_dir,name)
+        else:
+            self.log_path = '/tmp/data_logger.log'
 
-    def write(self,path):
-        with open(path,'wt') as fd:
-            self.write_prefix(fd)
-            fd.write('<' + TAG_LOG_EVENT + '>\n')
-            fd.write('<' + TAG_PERIOD + '>' + str(self.period) + '</' + TAG_PERIOD + '>\n')
-            fd.write('<' + TAG_ROLLOVER_COUNT + '>' + str(self.rollover_count) + '</' + TAG_ROLLOVER_COUNT + '>\n')
-            fd.write('</' + TAG_LOG_EVENT + '>\n')
-            self.write_suffix(fd)
-
+        plugins = self.root.find(TAG_PLUGINS)
+        for plugin_node in plugins:
+            new_plugin = PluginMeta() 
+            new_plugin.name = plugin_node.get('name')
+            schedule = plugin_node.find(TAG_SCHEDULE)
+            period_node = schedule.find(TAG_PERIOD)
+            new_plugin.period = int(period_node.text.strip())
+            rcount = schedule.find(TAG_ROLLOVER_COUNT)
+            if None is not rcount:
+                new_plugin.rollover_max = int(rcount.text.strip())
+            config_node = plugin_node.find(TAG_PLUGIN_CONFIG)
+            if None is not config_node:
+                new_plugin.config = et.tostring(config_node).decode('utf-8') 
+            self.plugins.append(new_plugin)
 
 class CcsLogger(object):
 
     def __init__(self):
         self.load_plugins()
+
+    def get_plugin_metadata(self,name):
+        global g_config
+        rv = None
+        for plugin_meta in g_config.plugins:
+            if name == plugin_meta.name:
+                rv = plugin_meta
+                break
+        return rv
 
     def load_plugins(self):
         self.plugins = list()
@@ -105,31 +131,45 @@ class CcsLogger(object):
                     name = SHARED_OBJECT_DIR + '.' + f
                     try:
                         mod = import_module(name)
-                        obj = mod.load()
-                        self.plugins.append(obj)
-                        log.info('Loaded plugin: ' + name)
-                    except Exception:
-                        log.error('Failed to load plugin: ' + name)
-                        pass
+                        if hasattr(mod,"load"):
+                            obj = mod.load()
+                            obj.plugin_name = f
+                            if hasattr(obj,'set_log_callback'):
+                                obj.set_log_callback(logmsg)
+                            plugin_meta = self.get_plugin_metadata(f)
+                            if None is not plugin_meta:
+                                if hasattr(obj,'set_config'):
+                                    obj.set_config(plugin_meta.config)
+                            obj.error_count = 0
+                            self.plugins.append(obj)
+                            logmsg(NAME,'Loaded plugin: ' + f)
+                        else:
+                            logmsg(NAME,'Plugin has no load function: ' + f)
+                    except Exception as ex:
+                        logmsg(NAME,'Failed to load plugin (' + f + '): ' + str(ex))
 
-    def collect(self,dstpath):
-        timestamp = datetime.utcnow()
+
+    def collect(self,plugin,plugin_meta):
+        plugin_meta.rollover_count += 1
+
+        if (plugin_meta.path == None) or (plugin_meta.rollover_count >= plugin_meta.rollover_max):
+            plugin_meta.path = self.get_collect_file_path(plugin.get_label())
+            plugin_meta.rollover_count = 0
+        timestamp = datetime.datetime.now(datetime.UTC)
         s = timestamp.strftime('%Y%m%d %I:%M:%S')
-        path = os.path.join(dstpath)
         header_written = True
-        with open(path,'a') as fd:
-            if 0 == os.path.getsize(path):
+        with open(plugin_meta.path,'a') as fd:
+            if 0 == os.path.getsize(plugin_meta.path):
                 header_written = False
-            for plugin in self.plugins:
-                data = plugin.get_current_values()
-                if False == header_written:
-                    header = self.get_header(data)
-                    fd.write(header + '\n')
-                    header_written = True
-                for x in data:
-                    if 2 == len(x):
-                        s += ',' + str(x[1])
-                fd.write(s + '\n')
+            data = plugin.get_current_values()
+            if False == header_written:
+                header = self.get_header(data)
+                fd.write(header + '\n')
+                header_written = True
+            for x in data:
+                if 2 == len(x):
+                    s += ',' + str(x[1])
+            fd.write(s + '\n')
 
     def get_header(self,dataset):
         s = 'Timestamp (UTC)'
@@ -137,35 +177,36 @@ class CcsLogger(object):
             s += ',' + str(x[0])
         return s
 
-def get_collect_file_path(dst):
-    ts = datetime.utcnow()
-    name = ts.strftime('%Y%m%d%I%M%S') + COLLECT_SUFFIX
-    return os.path.join(dst,name)
+    def get_collect_file_path(self,label):
+        global g_config
+        ts = datetime.datetime.now(datetime.UTC)
+        name = ts.strftime('%Y%m%d_%I%M%S') + '_' + label + COLLECT_SUFFIX
+        return os.path.join(g_config.data_dir,name)
 
 def run(args):
     global g_done
     global g_config
 
+    plugin_error_count = 0
     g_config = LoggerSettings()
     g_config.read(args.config)
 
-    if None is not args.period:
-        g_config.set_period(args.period)
-    if None is not args.events:
-        g_config.set_rollver_count(args.events)
-
     data_logger = CcsLogger()
-    count = 0
-    path = get_collect_file_path(g_config.data_dir)
-    print('path: ' + path + ', period: ' + str(args.period) + ', events: ' + str(args.events))
+    total_count = 0
     while False == g_done:
-        data_logger.collect(path)
-        time.sleep(60 * g_config.period)
-        count += 1
-        if count >= g_config.rollover_count:
-            # Get a new file with updated timestamp
-            path = get_collect_file_path(g_config.data_dir)
-            count = 0
+        for plugin in data_logger.plugins:
+            meta = data_logger.get_plugin_metadata(plugin.plugin_name)
+            if None is not meta:
+                meta.ticks += 1
+                if meta.ticks >= meta.period:
+                    data_logger.collect(plugin,meta)
+                    meta.ticks = 0
+            else:
+                if error_count < MAX_PLUGIN_REPORTED_ERRORS:
+                    logmsg(NAME,'Plugin is not configured properly: ' + plugin.get_label())
+                    error_count += 1
+        # 60 seconds per tick
+        time.sleep(60)
 
 
 if '__main__' == __name__:
