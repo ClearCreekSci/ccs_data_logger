2767ae2dc95c111da95983ad34a8e9453a590583
Winslow <winslow@clearcreeksci.com>
2026-05-26 15:58:17 -0400

    Added build_bundle.py

---
+++ deployment/build_bundle.py
@@ -0,0 +1,221 @@
+import argparse
+import os
+import subprocess
+import zipfile
+
+import datetime as dt
+import xml.etree.ElementTree as et
+
+from glob import glob
+
+DEFAULT_VERSION         = '1'
+DEFAULT_PREFIX          = 'install'
+
+MANIFEST_NAME           = 'manifest.xml'
+ZIP_SUFFIX              = '.zip'
+SCRIPT_SUFFIX           = '.sh'
+
+SCRIPT_LEN_REPLACE_STR  = '<***>'
+
+TAG_BASE                = 'paths'
+TAG_NAME                = 'name'
+TAG_PATHS               = 'paths'
+TAG_ROOT                = 'ccs-config'
+TAG_SENSOR              = 'sensor'
+TAG_SENSORS             = 'sensors'
+
+# FIXME: This needs to be configurable...
+TOPLEVEL_DST            = '/opt/ccs'
+SITE_DIR                = 'site-packages'
+DATALOGGER_DST          = TOPLEVEL_DST + '/DataLogger'
+SYSTEMD_SERVICE_DST     = '/etc/systemd/system'
+UNZIP_DST               = './unzip'
+SETTINGS_FILE_NAME      = './settings.cfg'
+
+class InvalidSettingsFileException(Exception):
+    pass
+
+class Settings(object):
+
+    def __init__(self):
+        self.paths = dict()
+        self.sensors = list()
+
+    def read(self,path):
+        tree = et.parse(path)
+        root = tree.getroot()
+        if root.tag == TAG_ROOT:
+            paths_node = root.find(TAG_PATHS)
+            if None is not paths_node:
+                for path_node in paths_node:
+                    name = path_node.tag.strip()
+                    value = path_node.text.strip()
+                    self.paths[name] = value
+            else:
+                raise InvalidSettingsFileException('No paths element in settings file: ' + str(path)) 
+            sensors_node = root.find(TAG_SENSORS)
+            if None is not sensors_node:
+                sensor_nodes = sensors_node.findall(TAG_SENSOR)
+                for sensor_node in sensor_nodes:
+                    name_node = sensor_node.find(TAG_NAME)
+                    if None is not name_node:
+                        name = name_node.tag.strip()
+                        self.sensors.append(name)
+                    else:
+                        raise InvalidSettingsFileException('Sensor has no name element in ' + str(path)) 
+            else:
+                raise InvalidSettingsFileException('No sensors element in settings file: ' + str(path)) 
+        else:
+            raise InvalidSettingsFileException(path + ' is not a valid settings file') 
+
+    def __repr__(self):
+        rv = ''
+        rv += 'paths: ' + str(self.paths) + '\n'
+        rv += 'sensors: ' + str(self.sensors) + '\n'
+        return rv
+
+def get_settings(path):
+    rv = None
+    if os.path.exists(path):
+        try:
+            rv = Settings()
+            rv.read(path)
+        except Exception as ex:
+            rv = None
+            print('Exception reading settings file: ' + str(ex))
+    else:
+        print("Couldn't find settings file: " + str(path))
+    return rv
+
+def add_glob_to_zip(zf,src,dst,glob_str):
+    files = glob(os.path.join(src,glob_str))
+    for f in files:
+        basename = os.path.basename(f)
+        dst_path = os.path.join(dst,basename)
+        zf.write(f,dst_path)
+
+# FIXME: WHAT ABOUT UNINSTALL?
+def create_base_script(zip_size,settings):
+    rv = ''
+    # Create the base script
+    rv = '#!/usr/bin/bash\n'
+    rv += 'if [ ! $EUID -eq 0 ]; then\n'
+    rv += '    echo "Please run this install script as root"\n'
+    rv += '    exit\n'
+    rv += 'fi\n'
+    rv += '# Make sure I2C is turned on...\n'
+    rv += 'echo "Enabling I2C..."\n'
+    rv += 'raspi-config nonint do_i2c 0\n'
+    rv += 'ME=$(basename "$0")\n'
+    rv += 'mkdir ' + UNZIP_DST + '\n'
+        # Extract the zip file from the install script
+    rv += 'dd bs=1 if="$ME" of=script.zip skip=' + SCRIPT_LEN_REPLACE_STR + ' count=' + str(zip_size) + '\n'
+    rv += 'unzip -q -d ' + UNZIP_DST + ' script.zip\n'
+    rv += '# Setup up the data logger files...\n'
+
+    for key in settings.paths.keys():
+        rv += 'mkdir -p ' + settings.paths[key] + '\n'
+
+    rv += 'echo "Copying data logger files..."\n'
+    rv += 'cp ' + UNZIP_DST + '/data_logger.py ' + DATALOGGER_DST + '\n'
+    rv += 'cp ' + UNZIP_DST + '/manifest.xml '  + DATALOGGER_DST + '\n'
+    rv += 'cp ' + UNZIP_DST + '/settings.cfg '  + DATALOGGER_DST + '\n'
+    rv += 'cp -r ' + UNZIP_DST + '/sensormods ' + DATALOGGER_DST + '\n'
+    rv += 'cp -r ' + UNZIP_DST + '/ccs_base ' + DATALOGGER_DST + '\n'
+    rv += 'cp -r ' + UNZIP_DST + '/ccs_dlconfig ' + DATALOGGER_DST + '\n'
+    rv += 'cp -r ' + UNZIP_DST + '/system/ccsdatalogger.service ' + SYSTEMD_SERVICE_DST + '\n'
+
+    rv += 'echo "Creating ccsdatalogger systemd service..."\n'
+    rv += 'systemctl daemon-reload\n'
+    rv += 'systemctl enable ccsdatalogger.service\n'
+    rv += 'systemctl start ccsdatalogger.service\n'
+
+    rv += 'rm -rf ' + UNZIP_DST + '\n'
+    rv += 'rm -rf script.zip\n'
+    rv += 'echo "Installation completed succesfully."\n'
+    rv += 'exit\n'
+    return rv
+
+def run(args):
+    global DATALOGGER_DST
+    commit = ''
+    prefix = DEFAULT_PREFIX
+    version = DEFAULT_VERSION
+    if None is not args.prefix:
+        prefix = args.prefix
+    if None is not args.version:
+        version = args.version
+    if None is not args.commit:
+        commit = args.commit
+    else:
+        # Popen call example...
+        # Source - https://stackoverflow.com/a/92395
+        # Posted by Eli Courtwright, modified by community. See post 'Timeline' for change history
+        # Retrieved 2026-05-22, License - CC BY-SA 4.0
+        commit = subprocess.Popen('git rev-parse HEAD', shell=True, stdout=subprocess.PIPE).stdout.read()
+        commit = str(commit).strip()
+
+    settings = get_settings(SETTINGS_FILE_NAME)
+    if None is settings:
+        return
+
+    if TAG_BASE in settings.paths.keys():
+        DATALOGGER_DST = settings.paths[TAG_BASE]
+
+    # Create the manifest
+    with open(MANIFEST_NAME,'wt') as fd:
+        fd.write('<manifest>\n')
+        current_time = dt.datetime.now(dt.timezone.utc).isoformat(timespec='minutes')
+        fd.write('<time>' + current_time + '</time>\n')
+        fd.write('<commit>' + commit + '</commit>\n')
+        fd.write('<version>' + str(version) + '</version>\n')
+        fd.write('</manifest>\n')
+
+    # Create the zip file
+    zip_name = str(prefix) + '_v' + str(version) + ZIP_SUFFIX
+    with zipfile.ZipFile(zip_name,mode='w') as zf:
+        zf.write('settings.cfg','settings.cfg')
+        zf.write('manifest.xml','manifest.xml')
+        zf.write('../data_logger.py','data_logger.py')
+        zf.mkdir('sensormods')
+        add_glob_to_zip(zf,'../sensormods','./sensormods','*.py')
+        zf.mkdir('ccs_base')
+        add_glob_to_zip(zf,'../ccs_base','./ccs_base','*.py')
+        zf.mkdir('ccs_dlconfig')
+        add_glob_to_zip(zf,'../ccs_dlconfig','./ccs_dlconfig','*.py')
+        zf.mkdir('system')
+        zf.write('./system/ccsdatalogger.service','system/ccsdatalogger.service')
+
+    zip_size = os.path.getsize(zip_name)
+
+    script = create_base_script(zip_size,settings)
+
+    base_len = len(script)
+    idx = script.find(SCRIPT_LEN_REPLACE_STR)
+    if idx > 0:
+        x = f'{base_len:05d}'
+        parts = script.split(SCRIPT_LEN_REPLACE_STR)
+        if 2 == len(parts):
+            script = parts[0] + x + parts[1]
+
+    # Concatenate the base script and the zip file
+    read_buf = ''
+    install_script_name = str(prefix) + '_logger_v' + str(version) + SCRIPT_SUFFIX
+    with open(install_script_name,'wb') as fd:
+        script = script.encode('utf-8')
+        fd.write(script)
+        with open(zip_name,'rb') as zfd:
+            zip_buf = zfd.read()
+        fd.write(zip_buf) 
+    
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v','--version',help='version string')
+    parser.add_argument('-c','--commit',help='commit string')
+    parser.add_argument('-p','--prefix',help='prefix string')
+    args = parser.parse_args()
+    run(args)
+
+
+
